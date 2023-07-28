5940cbc8493d9e6f5ed6b8eefd7c148ebe23495c
Sechkin Cholak <risetbme@gmail.com>
2023-07-28 11:01:11 +0300

    Fixed maven dependencies structure

---
+++ collections/src/main/java/ontotext/javacourse/collections/hashdice/HashDice.java
@@ -0,0 +1,11 @@
+package ontotext.javacourse.collections.hashdice;
+
+/**
+ * The HashDice class contains methods that take two dice with M sides that are rolled N times
+ * simultaneously and create statistics for the results.
+ */
+public class HashDice {
+  private HashDice() throws IllegalAccessException {
+    throw new IllegalAccessException("HashDice class is not meant to be instantiated");
+  }
+}
